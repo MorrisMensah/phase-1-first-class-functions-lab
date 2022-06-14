33b0ff2bcdf6b83b2ce22c0dbe2f779ff67a8f2d
Morris Mensah <morrismensah123@gmail.com>
2022-06-14 18:08:40 +0000

    Functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(number){
+    return function(){
+        return number * number;
+    }
+}
+
+const fareDoubler = function(fare){
+    return fare * 2;
+}
+
+const fareTripler = function(fare){return fare * 3;}
+
+function selectDifferentDrivers(drivers, selectingDrivers){
+    if(selectingDrivers === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers);
+    }
+    else{
+        return returnLastTwoDrivers(drivers);
+    }
+}
